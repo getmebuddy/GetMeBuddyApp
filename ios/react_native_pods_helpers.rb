c7bfcca4fc7b93eb943c7b8dfc359ebdcdd34eda
Vivek Singh <viki00762@gmail.com>
2025-03-05 16:15:31 -0600

    Update configs

---
+++ ios/react_native_pods_helpers.rb
@@ -0,0 +1,8 @@
+# Define missing helper functions
+def min_ios_version_supported
+    '15.0'
+  end
+  
+  def prepare_react_native_project!
+    # No-op - this is normally handled by the CLI
+  end
